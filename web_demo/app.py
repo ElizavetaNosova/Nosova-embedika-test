6ee9873faab63cc981b02809e8e855592cf78b05
ElizavetaNosova <lizanosova@yandex.ru>
2022-07-21 01:10:19 +0300

    Update app.py

--- web_demo/app.py
+++ web_demo/app.py
@@ -12,7 +12,7 @@ app.layout = html.Div([
     html.Div(dcc.Input(id='organization', type='text', placeholder=ORGANIZATION_PLACEHOLDER)),
     html.Button('Сгенерировать', id='submit-val', n_clicks=0),
     html.Div(id='container-button-basic',
-             children='Укажите персону (рекомендуем указывать не только фамилию, но и имя или инициалы) или организацию и нажмите на кнопку'),
+             children='Укажите персону (рекомендуем указывать не только фамилию, но и имя или инициалы) или организацию и нажмите на кнопку "Сгенерировать"'),
     html.Div(id="title", style={"font-weight": "bold"}),
     html.Div(id="text"),
 ])
@@ -24,10 +24,14 @@ app.layout = html.Div([
     State('person', 'value'),
     State('organization', 'value')
 )
-
 def update_output(n_clicks, person, organization):
+    if n_clicks == 0:
+        return '', ''
     person = person if person else ''
     organization = organization if organization else ''
     title, text = generate(person, organization)
     return title, text
 
+
+def clean_output(n_clicks):
+    return '', ''
