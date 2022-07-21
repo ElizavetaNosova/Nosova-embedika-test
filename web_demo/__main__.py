dd15c759730a0de155bfdbe3c391714927fc5180
ElizavetaNosova <lizanosova@yandex.ru>
2022-07-21 03:42:53 +0300

    readme + port

--- web_demo/__main__.py
+++ web_demo/__main__.py
@@ -1,4 +1,10 @@
 from .app import app
 
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument('--port',  type=int, default=8050)
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    args = parser.parse_args()
+    port = args.port
+    app.run(debug=False, port=port)
